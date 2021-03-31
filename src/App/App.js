9cbf3697464f1f0533cd819e206e702d07e3f9f7
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-03-31 10:58:09 +0530

    Added background to home page

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Home from '../Pages/Home/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <h1>Page not found!</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

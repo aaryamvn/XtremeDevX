5b91209e610b6b41b92ba62d30f35b6c983ef3a5
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-04-02 18:32:04 +0530

    v 1.4

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -21,7 +21,7 @@ const About = () => {
                             <div className="about__snippet-brackets-nested">
                                 <span className="about__snippet-pair"><span className="about__snippet-key">"name"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value">"Tejas Ravishankar"</span><span className="about__snippet-comma">,</span></span>
                                 <span className="about__snippet-pair"><span className="about__snippet-key">"handle"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value">"@XtremeDevX"</span><span className="about__snippet-comma">,</span></span>
-                                <span className="about__snippet-pair"><span className="about__snippet-key">"email"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value">"xtremedevx@gmail.com"</span><span className="about__snippet-comma">,</span></span>
+                                <span className="about__snippet-pair"><span className="about__snippet-key">"email"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value">"xtremedevx@electric.sh"</span><span className="about__snippet-comma">,</span></span>
                                 <span className="about__snippet-pair"><span className="about__snippet-key">"age"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value--num">14</span><span className="about__snippet-comma">,</span></span>
                                 <span className="about__snippet-pair"><span className="about__snippet-key">"isDeveloper"</span>  <span className="about__snippet-colon">:</span> <span className="about__snippet-value--bool">true</span><span className="about__snippet-comma">,</span></span>
                                 <span className="about__snippet-pair">
@@ -30,22 +30,22 @@ const About = () => {
                                     <span className="about__snippet-value--arr">
                                         <span className="about__snippet-array-bracket">[</span>
                                         <div className="about__snippet-array-brackets-nested">
-                                            <span className="about__snippet-value">"typescript"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"rust"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"python"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"react"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"rust"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"flutter"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"git"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"typescript"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"react"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"next.js"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"github"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"gitlab"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"mongodb"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"firebase"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"mongodb"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"realm"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"swift"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"node js"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"express"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"electron"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"node.js"</span><span className="about__snippet-comma">,</span> <br />
                                             <span className="about__snippet-value">"flask"</span><span className="about__snippet-comma">,</span> <br />
-                                            <span className="about__snippet-value">"c++"</span>
+                                            <span className="about__snippet-value">"express"</span><span className="about__snippet-comma">,</span> <br />
+                                            <span className="about__snippet-value">"electron"</span>
                                         </div>
                                         <span className="about__snippet-array-bracket">]</span>
                                     </span>
